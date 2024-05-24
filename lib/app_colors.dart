3da3feabc57b984f95ccaaa6bb3915c36d80d96d
Sayyoh <jumaboyevsamandar31072004@gmail.com>
2024-05-24 17:47:58 +0500

    Initial commit

---
+++ lib/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color audioMassageContainer0 = Color(0xFF6c60e4);
+  static const Color audioMassageContainer1 = Color(0xFF6670f4);
+  static const Color secondaryColor = Color(0xFF16222e);
+}
